72c7dc572d8837ca66a15fb9ffe10b91d8f44fa0
Amal Saud <amalsaud@Amals-MacBook-Air.local>
2022-08-16 23:12:11 +0300

    initial

---
+++ JavaProgram.java
@@ -0,0 +1,11 @@
+public class JavaProgram {
+    public static void main(String[] args) {
+        String name = "Amal";
+        int age = 11;
+        String hometown = "Jubail";
+
+        System.out.println("My name is " +name);
+        System.out.println("I am " +age+ " years old");
+        System.out.println("My hometown is " +hometown);
+    }
+}
